cbd747433bccca038cfdc4fd5803ad11cf685318
Jozark <joshua.meyer.personal@gmail.com>
2022-10-31 13:46:56 +0100

    create the cpi function

--- programs/puppet-master/src/lib.rs
+++ programs/puppet-master/src/lib.rs
@@ -1,15 +1,31 @@
 use anchor_lang::prelude::*;
+use puppet::cpi::accounts::SetData;
+use puppet::program::Puppet;
+use puppet::{self, Data};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L");
 
 #[program]
-pub mod puppet_master {
+mod puppet_master {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
+        puppet::cpi::set_data(ctx.accounts.set_data_ctx(), data)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct PullStrings<'info> {
+    #[account(mut)]
+    pub puppet: Account<'info, Data>,
+    pub puppet_program: Program<'info, Puppet>,
+}
+
+impl<'info> PullStrings<'info> {
+    pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetData<'info>> {
+        let cpi_program = self.puppet_program.to_account_info();
+        let cpi_accounts = SetData {
+            puppet: self.puppet.to_account_info()
+        };
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
